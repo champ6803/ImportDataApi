9ecb32b843daea56ddf04cf740fd00a62f852340
champ <champ6803@gmail.com>
2018-07-25 15:13:26 +0700

    create project

---
+++ Models/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GenerateUserApi.Models
+{
+    public class Profile
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string type_account { get; set; }
+        public int experience { get; set; }
+        public int age { get; set; }
+        public string skill { get; set; }
+    }
+}
